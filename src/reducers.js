d601a9b54c0dfdd8269585a71ec96971cdc14383
mikksillaste <mikk.sillaste@gmail.com>
2018-08-07 13:33:36 +0300

    Video 76(typo fixed)

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,10 @@
 import { CHANGE_SEARCH_FIELD } from "./constants";
 
-const initalState = {
+const initialState = {
     searchField: ''
 };
 
-export const searchRobots = (state=initalState, action={}) => {
+export const searchRobots = (state=initialState, action={}) => {
     switch (action.type) {
         case CHANGE_SEARCH_FIELD:
             return Object.assign({}, state, {searchField: action.payload});
