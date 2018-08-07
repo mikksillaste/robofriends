86c9054b9602bcef6b0528f2bd30c0316f2076f0
mikksillaste <mikk.sillaste@gmail.com>
2018-08-07 13:32:40 +0300

    Video 76

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
-import 'tachyons';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import { searchRobots } from "./reducers";
+import 'tachyons';
+
+const store = createStore(searchRobots);
 
 ReactDOM.render(
-    <App />, document.getElementById('root'));
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
