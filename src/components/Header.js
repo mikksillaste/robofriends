06e5bc4dc7d0fd5fecd4d0b485f576c33afa78fc
mikksillaste <mikk.sillaste@gmail.com>
2018-08-23 07:45:37 +0300

    Video 104(15:21)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import CounterButton from './CounterButton';
 
 class Header extends Component {
-    shouldComponentUpdate(nextProps, nextState) {
-        return false;
-    }
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return false
+    // }
 
     render() {
         console.log('Header');
-        return <h1 className='f1'>RoboFriends</h1>
+        return (
+            <div>
+                <h1 className='f1'>RoboFriends</h1>
+                <CounterButton color='red'/>
+            </div>
+        );
 
     }
 }
