16d5cecf3150a667bf83f14833d516517f5cdb78
mikksillaste <mikk.sillaste@gmail.com>
2018-09-07 07:23:59 +0300

    Video 147(0:00)

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,7 +1,12 @@
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import Card from './Card';
 
 it('expect to render card component', () => {
     expect(shallow(<Card />).length).toEqual(1);
+});
+
+// snapshot testing
+it('expect to render card component', () => {
+    expect(shallow(<Card />)).toMatchSnapshot();
 });
